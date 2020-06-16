2f14b8bd0f9ec19afb8d69b7a256c1f2b1fb3283
ettienekorayyi <corralstephenmelben@gmail.com>
2020-06-17 02:17:21 +1000

    Fixed descendant error with p tag.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,8 +7,7 @@ const AboutMe = () => {
   const onType = () => {
     return (
       <div id="text">
-        <p>
-          <TypeIt element={"p"}>
+        <TypeIt element={"p"}>
             I'm an aspiring fullstack software developer from New South Wales
             and I specialise in C#/React with a goal of becoming a fullstack web
             developer. I'm a fan of PS4 games, TV series and camping. <br></br><br></br>
@@ -16,7 +15,6 @@ const AboutMe = () => {
             I was a Customer Care Engineer(contractor) at Ricoh Australia for 8
             months and a software tester at Aabode for 10 months.
           </TypeIt>
-        </p>
       </div>
     );
   };
