314c4995ea811bbd4f6a14840ed78b86299cbf60
Stephen Melben Corral <corralstephenmelben@gmail.com>
2020-06-17 15:23:42 +1000

    Update AboutMe.js

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,19 +7,14 @@ const AboutMe = () => {
   const onType = () => {
     return (
       <div id="text">
-        <TypeIt
-          element={"p"}
-          getAfterInit={(instance) => {
-            instance.reset();
-            return instance;
-          }}
-        >
-          I'm an aspiring fullstack software developer from New South Wales and
-          I specialise in C#/React with a goal of becoming a fullstack web
-          developer. I'm a fan of PS4 games, TV series and camping. <br></br>
-          <br></br>I was a Customer Care Engineer(contractor) at Ricoh Australia
-          for 8 months and a software tester at Aabode for 10 months.
-        </TypeIt>
+        <TypeIt element={"p"}>
+            I'm an aspiring fullstack software developer from New South Wales
+            and I specialise in C#/React with a goal of becoming a fullstack web
+            developer. I'm a fan of PS4 games, TV series and camping. <br></br><br></br>
+
+            I was a Customer Care Engineer(contractor) at Ricoh Australia for 8
+            months and a software tester at Aabode for 10 months.
+          </TypeIt>
       </div>
     );
   };
