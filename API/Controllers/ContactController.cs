7f32a9f4630d9fb82a4fdde13823dd2916824560
ettienekorayyi <corralstephenmelben@gmail.com>
2020-07-29 13:41:57 +1000

    Restructured the projects to separate API and Client App

---
+++ API/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Domain> Get()
+        {
+            return new List<Domain>
+            {
+                new Domain {
+                    Age = 22,
+                    Name = "Stephen"
+                },
+                new Domain {
+                    Age = 25,
+                    Name = "Maggie"
+                }
+            };
+        }
+    }
+}
---
+++ API/Domain.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API
+{
+    public class Domain
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+    }
+}
